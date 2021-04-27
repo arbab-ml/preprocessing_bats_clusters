b95133d4846af10c1f6987de40fe0650686639ad
Arbab <arbab.arshad97@gmail.com>
2021-04-27 08:30:39 +0000

    sdaf

--- v3 precomputation all from batdetect.py
+++ v3 precomputation all from batdetect.py
@@ -26,7 +26,7 @@ from scipy.io import wavfile
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+import time
 from tqdm import tqdm
 from scipy.io.wavfile import read as read_wav
 import array
@@ -64,8 +64,9 @@ def plot_spectrogram(signals, labels_flag=False, rate=44100,mode="simple", targe
 	fig.suptitle('Spectrogram', size=16)
 	#axes.set_title(list(fbank.keys())[i])
 	hop_length_value=512
+	n_fft_value=5120
 	if (mode=="simple"):
-		spec = np.abs(librosa.stft(signals, hop_length=hop_length_value, n_fft=5120))
+		spec = np.abs(librosa.stft(signals, hop_length=hop_length_value, n_fft=n_fft_value))
 		spec = (librosa.amplitude_to_db(spec, ref=np.max))  #_normalize removed
 		axes=librosa.display.specshow(spec, sr=rate,hop_length=hop_length_value, x_axis='time', y_axis='linear') #when computing an STFT, you can pass that same parameter to specshow. 
 																					#This ensures that axis scales (e.g. time or frequency) are computed correctly.
@@ -86,7 +87,7 @@ def plot_spectrogram(signals, labels_flag=False, rate=44100,mode="simple", targe
 			wav_time_shift = np.r_[np.random.choice(signals, -start_ ), signals[:start_]]
 			
 		#^ processing done, now plotting
-		spec = np.abs(librosa.stft(wav_time_shift, hop_length=hop_length_value, n_fft=5120))
+		spec = np.abs(librosa.stft(wav_time_shift, hop_length=hop_length_value, n_fft=n_fft_value))
 		spec = (librosa.amplitude_to_db(spec, ref=np.max))#_normalize removed
 		axes=librosa.display.specshow(spec, sr=rate, x_axis='time',hop_length=hop_length_value, y_axis='linear')
 		ax2 = fig.gca() #getting this to set the range
@@ -107,7 +108,7 @@ def plot_spectrogram(signals, labels_flag=False, rate=44100,mode="simple", targe
 			cut_len = len(wav_speed_tune) - (target_length*rate)
 			wav_speed_tune = wav_speed_tune[int(cut_len/2):int(cut_len/2)+int(target_length*rate)]
 		#^ processing done, now plotting
-		spec = np.abs(librosa.stft(wav_speed_tune, hop_length=hop_length_value, n_fft=5120))
+		spec = np.abs(librosa.stft(wav_speed_tune, hop_length=hop_length_value, n_fft=n_fft_value))
 		spec = (librosa.amplitude_to_db(spec, ref=np.max))#_normalize removed
 		axes=librosa.display.specshow(spec, sr=rate, x_axis='time',hop_length=hop_length_value, y_axis='linear')
 		ax2 = fig.gca() #getting this to set the range
@@ -136,20 +137,31 @@ random.seed(5)
 all_stats=pd.DataFrame(columns=["file_name", "length", "sample rate"])
 
 bat_calls_data_raw=pd.read_csv(bat_calls_data_dir)
-bat_calls_data=bat_calls_data_raw[ bat_calls_data_raw["detection_prob"]>=0.99].reset_index()
+bat_calls_data=bat_calls_data_raw[ bat_calls_data_raw["detection_prob"]>=0.95].reset_index(drop=True)
 
 # bat_calls_data_processed=bat_calls_data
-bat_calls_data_processed=bat_calls_data.iloc[bat_calls_data.groupby('file_name')['detection_prob'].agg(pd.Series.idxmax)]
-# bat_calls_data_processed=bat_calls_data_processed.set_index("file_name")
+
+#selecting maximum probabiliyt value
+# bat_calls_data_processed=bat_calls_data.iloc[bat_calls_data.groupby('file_name')['detection_prob'].agg(pd.Series.idxmax)]
+
+#selecting from random place (given: probablity there is >=0.95)
+bat_calls_data_processed=bat_calls_data.groupby('file_name').agg(pd.DataFrame.sample).reset_index()
 
 for index, row_data in tqdm(bat_calls_data_processed.iterrows()):
 	try:
+		#giving CPU some rest
+		if (output_number_iterator%100==0):
+			print("Resting .. ")
+			time.sleep(7)
+
 		path=input_directory + row_data["file_name"]
 		save_to=row_data["file_name"].split('/')[-1]  +' at '+str(row_data["detection_time"])
 		#save_to= str(path.relative_to(input_directory)).split('/')[-1]
 		
 		#OLD METHOD--> signal, sr = librosa.load(path,sr=None)    #Explicitly Setting sr=None ensures original sampling preserved -- STOVF   
+
 		sound = AudioSegment.from_file(path)
+
 		if (sound.frame_rate!=target_sampling_rate):
 			sound=sound.set_frame_rate(target_sampling_rate)
 
@@ -159,8 +171,7 @@ for index, row_data in tqdm(bat_calls_data_processed.iterrows()):
 		signal = np.array(samples).astype(np.float32)
 		sr=sound.frame_rate
 
-
-
+				# your code here    
 
 		#Segmenting to a random value of 1 seconds (for initial experiemtation)
 		length= signal.shape[0]/sr 
@@ -190,12 +201,12 @@ for index, row_data in tqdm(bat_calls_data_processed.iterrows()):
 		signal=signal[int(start_value*sr):int(end_value*sr)]
 
 		new_length=len(signal)/sr
+		
 		plot_spectrogram(signal,rate=sr, mode="simple", target_length=new_length).savefig(output_directory+'/spectrograms_normalized/batsnet_train/1/'+save_to+'.png')
 		#plot_spectrogram(signal,rate=sr, mode="simple", target_length=length).savefig(output_directory+'/spectrograms/original/'+save_to+'.png')
 		plt.close()
 
 
-		
 		random_augmentation=random.randint(0, 1)
 		if(random_augmentation==0):
 			plot_spectrogram(signal,rate=sr, mode="timeshift", target_length=new_length).savefig(output_directory+'/spectrograms_normalized/augmented/'+save_to+'.png')
