46e107606beaaf3c306376f9cf79cff718df5cd0
arbab97 <arbab.arshad97@gmail.com>
2021-07-05 11:18:38 +0000

    5june

--- filename_to_image_rename.py
+++ filename_to_image_rename.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 import shutil
-input_data_directory="/media/rabi/Data/ThesisData/audio data analysis/audio-clustering/plots_15march_b/spectrograms_normalized/batsnet_train/1"
-input_file="//media/rabi/Data/ThesisData/audio data analysis/audio-clustering/plots_15march_b/spectrograms_normalized/results5april/results_SCAN.csv"
-output_data_directory="/media/rabi/Data/ThesisData/audio data analysis/audio-clustering/plots_15march_b/spectrograms_normalized/results5april/results_SCAN"
+input_data_directory="/content/drive/MyDrive/thesisdata/whole dataset/plots_26april/spectrograms_normalized/batsnet_train/1"
+input_file="/content/drive/MyDrive/thesisdata/whole dataset/plots_26april/spectrograms_normalized/results/results_jule.csv"
+output_data_directory="/content/drive/MyDrive/thesisdata/whole dataset/plots_26april/spectrograms_normalized/results/results_jule"
 
 
 input_data=pd.read_csv(input_file)
@@ -11,7 +11,7 @@ input_data=pd.read_csv(input_file)
 #further processing for kaleidoscope
 
 
-
+counter=0
 for index, (_, file_path, cluster_assigned) in input_data.iterrows():
     file_name=file_path.split('/')[-1]
     copy_this_file=os.path.join(input_data_directory, file_name)
@@ -19,3 +19,6 @@ for index, (_, file_path, cluster_assigned) in input_data.iterrows():
     os.makedirs(output_data_directory_local, exist_ok=True)
     to_this_path=os.path.join(output_data_directory_local, (str(cluster_assigned)+ "-Cluster:"+file_name)) 
     shutil.copy2(copy_this_file,to_this_path )
+    counter=counter+1
+    if not (counter%50):
+      print(counter)
